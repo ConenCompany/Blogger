577bbed3b4440a46f9d4e207afa6f81c33a62e76
ConenCompany <68103094+ConenCompany@users.noreply.github.com>
2020-11-13 18:41:02 +0100

    test1

---
+++ js/login.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    const form = document.getElementById('login-form');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault()
+
+        const username = document.getElementById('username').value
+        const password = document.getElementById('password').value
+
+        const users = [
+            {
+                username: 'Gerd',
+                password: 'Conen'
+           }
+       ]
+        
+        const user = users.find(function(user) {
+            return user.username === username
+        })
+
+        if (user && user.password === password) {
+            window.localStorage.setItem('loggedIn', true)
+            window.location.pathname = '/ConenCompany.github.io/index.html'
+        } else {
+            document.getElementById('login-failed').classList.remove('d-none')
+        }
+
+    })
+
+})
---
+++ js/main.js
@@ -0,0 +1,37 @@
+
+const responsive ={
+    0 : {
+        items : 1
+    },
+    320 :{
+        items : 1
+    },
+    560 :{
+        items: 2
+    },
+    800 : {
+        items: 3
+    }
+}
+$(document).ready(function () {
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse')
+
+
+    /** click event on toggle menu */
+    $toggleCollapse.click(function () {
+        $nav.toggleClass('collapse')
+    })
+    // owl carousel for blog
+    $('.owl-carousel').owlCarousel({
+        margin:30,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        dots:true,
+        nav:true,
+        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+        responsive : responsive
+    });
+});
