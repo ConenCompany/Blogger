a9e39670fde9128ee397af0cbefc1a5a2f74a78d
ConenCompany <68103094+ConenCompany@users.noreply.github.com>
2020-12-12 01:10:11 +0100

    update5647646237

--- js/login.js
+++ js/login.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
         if (user && user.password === password) {
             window.localStorage.setItem('loggedIn', true)
-            window.location.pathname = '/index.html'
+            window.location.pathname = '/index1.html'
         } else {
             document.getElementById('login-failed').classList.remove('d-none')
         }
