c5480e79c0f8074772bd1072bc0c9665c5fbb86e
ConenCompany <68103094+ConenCompany@users.noreply.github.com>
2021-07-14 11:00:24 +0200

    sergsedtgsetd

--- js/loginNew.js
+++ js/loginNew.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         const users = [
             {
                 username: 'Gerd',
-                password: 'Conen'
+                password: 'GerdConen1000='
            }
        ]
         
