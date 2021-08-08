3a050aa1929a80381fd11f47cbd79cf652ec39fd
ConenCompany <68103094+ConenCompany@users.noreply.github.com>
2021-08-08 14:48:04 +0200

    iusehruihseg

--- Programm/test_mqtt_Publisher_javascript.js
+++ Programm/test_mqtt_Publisher_javascript.js
@@ -4,12 +4,19 @@ var client = mqtt.connect('mqtt://test.mosquitto.org')
 client.on('connect', function () {
     console.log('Connection has been established successfully!');
 
-    client.subscribe('shellies/shellydimmer2-E8DB84D6A2CA/temperature');
-    client.subscribe('shellies/shellydimmer2-E8DB84D6A2CA/light/0/status');
-    client.subscribe('shellies/shellydimmer2-E8DB84D6A2CA/light/0');
-    client.publish("shellies/shellydimmer2-E8DB84D6A2CA/light/0/command", "on", function () {
-        console.log("sent ==> ", "on");
-    });
+    //client.subscribe('shellies/shellydimmer2-E8DB84D6A2CA/temperature');
+    //client.subscribe('shellies/shellydimmer2-E8DB84D6A2CA/light/0/status');
+    //client.subscribe('shellies/shellydimmer2-E8DB84D6A2CA/light/0');
+    //client.subscribe('esp32/distance/hcsr04/Value');
+    client.subscribe('shellies/shellymotion-60A42386D374/status');
+    client.subscribe('shellies/shellygas-E868E7C6E54A/sensor/operation');
+    client.subscribe('shellies/shellygas-E868E7C6E54A/sensor/gas');
+    client.subscribe('shellies/shellygas-E868E7C6E54A/sensor/concentration');
+    client.subscribe('esp32/distance/hcsr04/Value');
+    //client.publish('shellies/shellydimmer2-E8DB84D6A2CA/light/0/command', "on");
+    //client.publish("shellies/shellydimmer2-E8DB84D6A2CA/light/0/command", "on", function () {
+       // console.log("sent ==> ", "on");
+    //});
 
     
 });
